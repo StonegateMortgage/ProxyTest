6449300c1f4827e8669439b9e2599449278c14b7
Jim Karg <jkarg@stonegatemtg.com>
2014-10-23 13:09:11 -0500

    Upgraded to using express routing

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    var port = process.env.port || 8009;
+  var port = process.env.port || 8009;
 
-    var http = require('http'),
-        httpProxy = require('http-proxy');
+  var http = require('http'),
+    httpProxy = require('http-proxy');
 
-    httpProxy.createServer({
-        target: 'https://dev-api.stonegateconnect.com/'
-        //secure: true
-    }).listen(port);
+  var express = require('express');
+  var app = express();
+  var serveStatic = require('serve-static');
+  var finalhandler = require('finalhandler')
+
+  var serve = serveStatic('./', {
+    'index': ['index.html']
+  });
+
+  app.use(serve);
+
+  var apiProxy = httpProxy.createProxyServer({
+    target: 'https://dev-api.stonegateconnect.com'
+  });
+
+  app.all("/api/*", function(req, res) {
+    apiProxy.web(req, res);
+  });
+
+  app.all("/Token", function(req, res) {
+    apiProxy.web(req, res);
+  });
+
+  // Listen
+  app.listen(port);
 })();
