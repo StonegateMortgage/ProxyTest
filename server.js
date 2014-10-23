70efcf4327dff10837689a892d132a26ecc21655
Jim Karg <jkarg@stonegatemtg.com>
2014-10-23 12:41:53 -0500

    Updated to use environment port

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 (function () {
     'use strict';
 
+    var port = process.env.port || 8009;
+
     var http = require('http'),
         httpProxy = require('http-proxy');
 
     httpProxy.createServer({
         target: 'https://dev-api.stonegateconnect.com/'
         //secure: true
-    }).listen(8009);
+    }).listen(port);
 })();
